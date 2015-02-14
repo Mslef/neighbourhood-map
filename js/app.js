bf3eaea2ee0fad7f27e8539617065dd1e08e4807
Cheryl Court <cheryl.court@gmail.com>
2015-02-14 16:47:31 -0700

    Used jQuery selection to select map-canvas.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 function initialize() {
+
     var mapOptions = {
-        center: { lat: 46.097276, lng: -64.711679},
+        center: { lat: 46.097276, lng: -64.711679 },
         zoom: 15
     };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+
+    var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
