bf4b11cb8c35519e25801e4f66d42a8105035a7f
Cheryl Court <cheryl.court@gmail.com>
2015-02-15 19:14:55 -0700

    Changed map location to Edmonton; added a marker.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 function initialize() {
 
     var mapOptions = {
-        center: { lat: 46.097276, lng: -64.711679 },
+        center: { lat: 53.543256, lng: -113.501459 },
         zoom: 15
     };
 
     var map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+
+    var craftLatLng = new google.maps.LatLng(53.5433839,-113.5018665);
+
+    var marker = new google.maps.Marker({
+        position: craftLatLng,
+        map: map,
+        title: "Craft Beer Market"
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
