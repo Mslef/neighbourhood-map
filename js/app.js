513589a830acb94cb3bc92c3a28ebb2877035d25
Cheryl Court <cheryl.court@gmail.com>
2015-02-21 14:19:46 -0700

    Retrieved results for restaurants from google search, created markers and built array of place objects.

--- js/app.js
+++ js/app.js
@@ -1,68 +1,50 @@
-var places = [
-    {
-        title: 'Craft Beer Market',
-        id: 'ChIJSdYGvcs8oFMRzHh-vtHsKNQ',
-        location: {lat: 53.542050, lng: -113.491300}
-    },
-    {
-        title: 'Citadel Theatre',
-        id: 'ChIJlc4ee0UioFMRhOLD__mjUr4',
-        location: {lat:53.542451, lng: -113.48879999999997}
-    },
-    {
-        title: 'Winspear Center',
-        id: 'ChIJVdAXVUQioFMRF0G6jDBCG7o',
-        location: {lat:53.543900, lng: -113.488215}
-    },
-    {
-        title: 'Alberta Art Gallery',
-        id: 'ChIJkwb3ks6HblMRnGcEDs3gUfg',
-        location: {lat:49.696143, lng: -112.83840900000001}
-    },
-    {
-        title: 'Edmonton Public Library',
-        id: 'ChIJuyltKBAioFMRQW8csX-c4es',
-        location: {lat:53.519455, lng: -113.497479}
-    },
-    {
-        title: 'Haweli',
-        id: 'ChIJqSQLf-QloFMRfDJdJHdUcEE',
-        location: {lat: 53.649489, lng: -113.62926299999998}
-    },
-    {
-        title: 'The Fairmont Hotel MacDonald',
-        id: 'ChIJ25aWN0QioFMR1et7Rl46mF0',
-        location: {lat:53.540393, lng: -113.48965900000002}
-    },
-    {
-        title: 'Blue Plate Diner',
-        id: 'ChIJE2mky6kjoFMRS2R6aPn3omY',
-        location: {lat:53.542912, lng: -113.49910799999998}
-    },
-    {
-        title: 'Credo',
-        id: 'ChIJXcuUsdQjoFMRepcef8HD3t8',
-        location: {lat: 53.546115, lng: -113.536159}
-    }
-];
+var map;
+var service;
+var places = [];
 
 function initialize() {
 
     var mapOptions = {
-        center: { lat: 53.543256, lng: -113.501459 },
-        zoom: 16
+        center: { lat: 53.543256, lng: -113.496000 },
+        zoom: 17
     };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    for(var i = 0; i < places.length; i++) {
-        new google.maps.Marker({
-            position: places[i].location,
-            map: map,
-            title: places[i].title
-        });
+    infoWindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
+    google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
+
+function performSearch() {
+    var request = {
+        bounds: map.getBounds(),
+        keyword: 'restaurant'
+    };
+    service.nearbySearch(request, callback);
+}
+
+function callback(results, status, pagination) {
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
     }
 
+    for (var i = 0, result; result = results[i]; i++) {
+        var marker = new google.maps.Marker({
+            position: result.geometry.location,
+            title: result.name
+        });
+
+        places.push({
+            title: result.name,
+            id: result.place_id,
+            vicinity: result.vicinity,
+            marker: marker
+        });
+
+        marker.setMap(map);
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
